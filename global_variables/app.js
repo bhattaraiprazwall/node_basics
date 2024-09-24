874baec7ee5b1d5a3dce8696afbe188c4e8a8619
bhattaraiprazwall <youremail@gmail.com>
2024-09-24 19:33:12 +0545

    working on the root file of global variables

---
+++ global_variables/app.js
@@ -0,0 +1,13 @@
+// __dirname Global variable
+console.log(__dirname);
+
+// __filename Global variable
+console.log(__filename);
+
+
+// creating own global variables accessible from anywhere in the code
+global.myVariable="Hello world"; //defining a global variable "myVariable"
+console.log(myVariable); //accessing the global variable
+
+
+
