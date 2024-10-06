8e83f1a54de7189a9c38fd9a749cb27142842b9b
bhattaraiprazwall <youremail@gmail.com>
2024-10-06 07:04:29 +0545

    working on modules in node js

--- modules/app.js
+++ modules/app.js
@@ -1,4 +1,8 @@
-const sayHello=require('./modules.js');
-sayHello("ram");
-sayHello("hari");
-sayHello("gita");
+// const sayHello=require('./modules.js');
+// sayHello("ram");
+// sayHello("hari");
+// sayHello("gita");
+
+// importing modules.js second module and using the moduleExporting() function here
+const moduleExporting=require('./modules.js');
+moduleExporting();
