456fcaba3465e85322bb9d8af004400a6d73baca
bhattaraiprazwall <youremail@gmail.com>
2024-10-06 07:09:28 +0545

    creating modules assigning to an object

---
+++ module.exports/mymodule.js
@@ -0,0 +1,13 @@
+function myFunction1()
+{
+        console.log("Hello from myfunction 1");
+}
+function myFunction2()
+{
+    console.log("hello from myfunction 2");
+}
+module.exports={
+    foo:'bar',
+    myFunction1:myFunction1,
+    myFunction2:myFunction2
+};
